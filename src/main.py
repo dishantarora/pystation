881ac1a6968eafd2b989d03194f8b8f21194ad40
Dishant Arora <dishantarora@outlook.com>
2023-08-06 16:52:36 +0530

    Updated main.py using newly installed tabnine on vscode.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print('Hello World');
+def main():
+    ''' This function is called when the application is started. '''
+    
+    message = "Hello World from my first python project on Github."
+    print(message)
+
+if __name__ == "__main__":
+    main()
