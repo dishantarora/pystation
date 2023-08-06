e187312098ad240943b955ad4dd3d83a7c3aa944
Dishant Arora <dishantarora@outlook.com>
2023-08-06 16:55:57 +0530

    Update main.py
    Added final empty line.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 def main():
     ''' This function is called when the application is started. '''
-    
+
     message = "Hello World from my first python project on Github."
     print(message)
 
 if __name__ == "__main__":
     main()
+    
